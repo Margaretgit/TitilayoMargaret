1d37233c92aed66a4e56f2ea42646807997fe6bb
Eseimokumo Titilayo <t.eseimokumo@gmail.com>
2020-03-23 22:17:18 +0100

    my JavaScript Task

---
+++ script.js
@@ -0,0 +1,17 @@
+ let name = "Titilayo";
+
+ console.log("My name is: " + name);
+
+ const data = ['coding', 'Html', 'css', 'Javascript'];
+
+ for (let i=0; i<=200; i++) {
+
+   if (i%2===0) {
+
+     data.push(i);
+
+   }
+
+ }
+
+ console.log(data);
